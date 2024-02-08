bd18aad8051c3980cc7777fb8099ad1a44a54c8e
Lucas Nogueira Lopes <lucasnog@msn.com>
2024-02-08 09:54:33 -0300

    Ajuste map thread-safe

--- src/main/java/com/lucasnogueira/onebrc/springbatch/impl/CalculoMap.java
+++ src/main/java/com/lucasnogueira/onebrc/springbatch/impl/CalculoMap.java
@@ -4,13 +4,13 @@ import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 @Data
 @Component
 public class CalculoMap {
 
-    private final Map<String, Calculo> mapa = new TreeMap<>();
+    private final Map<String, Calculo> mapa = new ConcurrentSkipListMap<>();
 
 
 
@@ -22,3 +22,4 @@ public class CalculoMap {
         mapa.put(chave, calculo);
     }
 }
+
